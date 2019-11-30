ad3085d93732f94737ae158403704894ae8d8776
WitchesMe <54588062+WitchesMe@users.noreply.github.com>
2019-11-30 21:21:15 +0530

    Update cannon.js

--- cannon.js
+++ cannon.js
@@ -11,27 +11,24 @@ class Cannon{
 
     display(){
         var pos = this.body.position;
-        //var angle = this.body.angle;
-        if(keyIsDown(LEFT_ARROW)){
-            angle -=1
+        if(keyIsDown(LEFT_ARROW) && angle > -55){
+            angle = angle - 5;
+            
         }
-        if(keyIsDown(RIGHT_ARROW)){
-            angle +=1
+        if(keyIsDown(RIGHT_ARROW) && angle < 0){
+            angle = angle + 5;
+            
         }
         push();
         fill(255);
         translate(pos.x, pos.y);
-        //rectMode(CENTER);
         rotate(angle);
-        strokeWeight(2);
         //Create the shooter from which the cannonballs will get dispersed
         rect(0, 0, this.width, this.height);
         pop();
-        
         fill(255);
-        strokeWeight(2);
         //Create the semicircle above the two rectangles 
-        arc(70, 465, 90, 90, 180, 360);
+        arc(70, 465, 90, 100, 180, 360);
 
         //Create the two rectangles below the semicircle
         rect(20, height-85, 100, 20);
