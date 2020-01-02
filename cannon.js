da5977ed842e5a38f243af1be9d08ded404acfe0
mecoding4fun <53414760+mecoding4fun@users.noreply.github.com>
2020-01-02 18:48:53 +0530

    Update cannon.js

--- cannon.js
+++ cannon.js
@@ -1,37 +1,47 @@
 class Cannon{
-    constructor(x, y, width, height){
-        var options = {
-            isStatic: true,
+    constructor(bodyA,bodyB){
+
+        var constraintoptions = {
+       bodyA:bodyA,
+       bodyB:bodyB,
+    
+
         }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
+      //  this.body = Bodies.rectangle(75,height-125,60,30, options);
+        this.cannon = Constraint.create(constraintoptions);
+     /*   this.width = width;
         this.height = height;
-        World.add(world, this.body);
+        World.add(world, this.body);*/
+        World.add(world,this.cannon);
     };
 
-    display(){
-        var pos = this.body.position;
-        if(keyIsDown(LEFT_ARROW) && angle > -55){
-            angle = angle - 5;
-            
-        }
-        if(keyIsDown(RIGHT_ARROW) && angle < 0){
-            angle = angle + 5;
-            
-        }
+    attach(body){
+        this.cannon.bodyA = body;
+    }
+
+    shoot(){
+        this.cannon.bodyA = null;
+    }
+
+
+  /*  display(){
+     //   var pos = this.body.position;
+
         push();
         fill(255);
-        translate(pos.x, pos.y);
+       // translate(pos.x, pos.y);
         rotate(angle);
         //Create the shooter from which the cannonballs will get dispersed
         rect(0, 0, this.width, this.height);
         pop();
         fill(255);
-        //Create the semicircle above the two rectangles 
+
         arc(70, 465, 90, 100, 180, 360);
 
-        //Create the two rectangles below the semicircle
         rect(20, height-85, 100, 20);
         rect(20, height-65, 100, 20);
-    }
-}
+
+ 
+
+    } */
+}   
