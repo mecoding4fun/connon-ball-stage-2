366484bc52eceb087aad5ff506f1f32483feed8b
mecoding4fun <53414760+mecoding4fun@users.noreply.github.com>
2020-01-03 19:07:53 +0530

    Update cannon.js

--- cannon.js
+++ cannon.js
@@ -1,8 +1,8 @@
 class Cannon{
-    constructor(bodyA,bodyB){
+    constructor(pointA,bodyB){
 
         var constraintoptions = {
-       bodyA:bodyA,
+            pointA:pointA,
        bodyB:bodyB,
     
 
@@ -16,32 +16,13 @@ class Cannon{
     };
 
     attach(body){
-        this.cannon.bodyA = body;
+        this.cannon.pointA = body;
     }
 
     shoot(){
-        this.cannon.bodyA = null;
+        this.cannon.pointA = null;
     }
 
 
-  /*  display(){
-     //   var pos = this.body.position;
 
-        push();
-        fill(255);
-       // translate(pos.x, pos.y);
-        rotate(angle);
-        //Create the shooter from which the cannonballs will get dispersed
-        rect(0, 0, this.width, this.height);
-        pop();
-        fill(255);
-
-        arc(70, 465, 90, 100, 180, 360);
-
-        rect(20, height-85, 100, 20);
-        rect(20, height-65, 100, 20);
-
- 
-
-    } */
 }   
