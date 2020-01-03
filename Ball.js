76109393b6054e902efa68f23c55070884c8b56b
mecoding4fun <53414760+mecoding4fun@users.noreply.github.com>
2020-01-03 19:10:08 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -17,21 +17,21 @@ display(){
     //this.body.position.x = mouseX;
   //  this.body.position.y = mouseY;
 
-    image(this.image,120,height-125);
     var angle = this.body.angle;
     push();
     translate(this.body.position.x,this.body.position.y);
     rotate(angle);
     imageMode(CENTER);
+    image(this.image,120,height-125,10,10);
    //console.log(mouse.x);
     
    //image(this.image,0,0,this.width,this.height);
     pop();
-    if(this.body.velocity.x > 10 && this.body.position.x > 200){
-        var position = [this.body.position.x, this.body.position.y];
+    // if(this.body.velocity.x > 10 && this.body.position.x > 200){
+    //     var position = [this.body.position.x, this.body.position.y];
 
     
-    }
+    // }
     
 
 
